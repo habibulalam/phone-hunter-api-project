38a5130a9c2dd57c993f5489de77714f5bdd3af6
habibulalam <habibhassan.stt20@gmail.com>
2024-03-02 13:27:06 +0600

    html structure created and api called

---
+++ js/phones.js
@@ -0,0 +1,36 @@
+const loadPhone = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    const phones = data.data;
+     displayPhones(phones); 
+}
+
+const displayPhones = (phones) => {
+    const phoneContainer = document.getElementById('card-container')
+    phones.forEach(phone => {
+        // console.log(phone)
+        const phoneCard = document.createElement('div')
+        phoneCard.classList = `card bg-gray-100 p-3 shadow-xl`;
+        phoneCard.innerHTML = `
+            <figure><img src="${phone.image}" alt="Shoes" /></figure>
+                <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+        `
+        phoneContainer.appendChild(phoneCard);
+
+    })
+}
+
+// Search input
+const searchPhone = () => {
+    const inputField = document.getElementById('search')
+    const inputText = inputField.value;
+    console.log(inputText)
+}
+
+loadPhone();
